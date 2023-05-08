495fb242f4016cd200a424baaa03dbca10a6f940
Ewald <ewaldhorn@gmail.com>
2023-05-08 11:59:47 +0000

    Before challenge 2

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,12 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare float and convert it to an unsigned int
+	var myFloat = 17.05
+	var myInt = uint(myFloat)
+
+	fmt.Println("myFloat, myInt:", myFloat, myInt)
 }
